da6a81e2af8c783ef22a7ac9212f71c7de8a4b8d
felderth <fldrth@gmail.com>
2018-06-05 10:01:20 +0200

    Bug fix decision table

--- src/main/java/ch/fhnw/digibp/eiger/TaskExecutionListener.java
+++ src/main/java/ch/fhnw/digibp/eiger/TaskExecutionListener.java
@@ -9,12 +9,12 @@ import org.camunda.bpm.engine.delegate.TaskListener;
 
 public class TaskExecutionListener implements TaskListener {
 	
-	private Expression candidateDecision;
+	private Expression applicationEligible;
 	
 	public void notify(DelegateTask delegateTask) {
-		System.out.println("candidateDecision: " + candidateDecision.getValue(delegateTask).toString());
+		System.out.println("applicationEligible: " + applicationEligible.getValue(delegateTask).toString());
 		System.out.println("applicationEligible: " + delegateTask.getExecution().getVariable("applicationEligible"));
-	      if("true".equals(candidateDecision.getValue(delegateTask).toString())) {
+	      if("true".equals(applicationEligible.getValue(delegateTask).toString())) {
 	    	  delegateTask.getExecution().setVariable("applicantsInvited", true);	
 	      }
 	    System.out.println("applicantsInvited: " + delegateTask.getExecution().getVariable("applicantsInvited"));
